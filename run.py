a2527834c63e5e75b6c46ebd9695eaf14983f628
Matej Zidek <matej@zidek.eu>
2019-02-28 18:59:30 +0000

    fixing run.py

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import importlib
 
 def get_score(filename) -> int:
     score = 0
+    with open(filename) as f:
+        slide_count = f.readline()
     return score
 
 
@@ -11,8 +13,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Usage: python run.py <input filename> <algorithm filename>")
     parser.add_argument('input_file', type=str, help='Input filename')
     parser.add_argument('alg_file', type=str,
-                        help='Filename of the algorithm; the algorithm needs to have run(filename) function'
-                             'which returns name of the output file')
+                        help='Filename of the algorithm without ".py"; the algorithm needs to have run(filename)'
+                             'function which returns name of the output file')
     args = parser.parse_args()
     alg = importlib.import_module(args.alg_file)
     output_file = alg.run(args.input_file)
